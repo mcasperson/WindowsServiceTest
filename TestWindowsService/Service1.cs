eefb0fb52e9d398f9dc91b90fe64095501f85f2e
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-26 09:06:18 +1000

    Initial Import

--- TestWindowsService/Service1.cs
+++ TestWindowsService/Service1.cs
@@ -15,14 +15,23 @@ namespace TestWindowsService
         public Service1()
         {
             InitializeComponent();
+            eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("TestService"))
+            {
+                EventLog.CreateEventSource("TestService", "TestServiceLog");
+            }
+            eventLog1.Source = "TestService";
+            eventLog1.Log = "TestServiceLog";
         }
 
         protected override void OnStart(string[] args)
         {
+            eventLog1.WriteEntry("In OnStart for good service");
         }
 
         protected override void OnStop()
         {
+            eventLog1.WriteEntry("In OnStop for good service");
         }
     }
 }
